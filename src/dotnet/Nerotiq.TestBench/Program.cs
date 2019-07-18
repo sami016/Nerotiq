cb87e29854c800818cd9b9e4a8afd5d4f64d56ec
Sam Holder <samholder@live.co.uk>
2019-07-18 18:39:40 +0100

    Added based project structure.

---
+++ src/dotnet/Nerotiq.TestBench/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using OpenCL.Net;
+using OpenCL.Net.Extensions;
+
+namespace Nerotiq.TestBench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            uint platformCount;
+            ErrorCode result = Cl.GetPlatformIDs(0, null, out platformCount);
+            Console.WriteLine("{0} platforms found", platformCount);
+            
+
+            var platformIds = new Platform[platformCount];
+            result = Cl.GetPlatformIDs(platformCount, platformIds, out platformCount);
+            var platformCounter = 0;
+            foreach (var platformId in platformIds) {
+                IntPtr paramSize;
+                result = Cl.GetPlatformInfo(platformId, PlatformInfo.Name, IntPtr.Zero, InfoBuffer.Empty, out paramSize);
+
+                using (var buffer = new InfoBuffer(paramSize))
+                {
+                    result = Cl.GetPlatformInfo(platformIds[0], PlatformInfo.Name, paramSize, buffer, out paramSize);
+
+                    Console.WriteLine($"Platform {platformCounter}: {buffer}");
+                }
+                platformCounter++;
+            }
+
+            Console.WriteLine($"Using first platform...");
+            
+            uint deviceCount;
+            result = Cl.GetDeviceIDs(platformIds[0], DeviceType.All, 0, null, out deviceCount);
+            Console.WriteLine("{0} devices found", deviceCount);
+
+            var deviceIds = new Device[deviceCount];
+            result = Cl.GetDeviceIDs(platformIds[0], DeviceType.All, deviceCount, deviceIds, out var numberDevices);
+            
+            var selectedDevice = deviceIds[0];
+
+            var context = Cl.CreateContext(null, 1, new[] { selectedDevice }, null, IntPtr.Zero, out var error);
+
+            const string correctSource = @"
+            // Simple test; c[i] = a[i] + b[i]
+            __kernel void add_array(__global float *a, __global float *b, __global float *c)
+            {
+                int xid = get_global_id(0);
+                c[xid] = a[xid] + b[xid];
+            }
+            
+            __kernel void sub_array(__global float *a, __global float *b, __global float *c)
+            {
+                int xid = get_global_id(0);
+                c[xid] = a[xid] - b[xid];
+            }
+            ";
+
+                
+            var program = Cl.CreateProgramWithSource(context, 1, new[] { correctSource }, null, out var error2);
+            error2 = Cl.BuildProgram(program, 1, new[] { selectedDevice }, string.Empty, null, IntPtr.Zero);
+    
+            Console.WriteLine(error2);
+
+            program.Dispose();
+        }
+    }
+}
---
+++ src/dotnet/Nerotiq/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nerotiq
+{
+    public class Class1
+    {
+    }
+}
