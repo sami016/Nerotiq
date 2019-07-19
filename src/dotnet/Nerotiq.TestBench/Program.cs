bb3f5140a1e213d224b5705b60c34f12ee28ec85
Sam Holder <samholder@live.co.uk>
2019-07-19 19:43:00 +0100

    Basic opencl test.

--- src/dotnet/Nerotiq.TestBench/Program.cs
+++ src/dotnet/Nerotiq.TestBench/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using OpenCL.Net;
 using OpenCL.Net.Extensions;
 
@@ -64,6 +65,58 @@ namespace Nerotiq.TestBench
     
             Console.WriteLine(error2);
 
+            // Get the kernels.
+            var kernels = Cl.CreateKernelsInProgram(program, out error);
+            Console.WriteLine($"Program contains {kernels.Length} kernels.");
+            var kernel = kernels[0];
+
+            //
+            float[] A = new float[1000];
+            float[] B = new float[1000];
+            float[] C = new float[1000];
+
+            for (var i=0; i<1000; i++) {
+                A[i] = i;
+                B[i] = i;
+            }
+
+            IMem<float> hDeviceMemA = Cl.CreateBuffer(context, MemFlags.CopyHostPtr | MemFlags.ReadOnly, A, out error);
+            IMem<float> hDeviceMemB = Cl.CreateBuffer(context, MemFlags.CopyHostPtr | MemFlags.ReadOnly, B, out error);
+            IMem<float> hDeviceMemC = Cl.CreateBuffer(context, MemFlags.CopyHostPtr | MemFlags.ReadOnly, C, out error);
+
+            // Create a command queue.
+            var cmdQueue = Cl.CreateCommandQueue(context, selectedDevice, CommandQueueProperties.None, out error);
+
+            
+            int intPtrSize = 0;
+            intPtrSize = Marshal.SizeOf(typeof(IntPtr));
+
+            error = Cl.SetKernelArg(kernel, 0, new IntPtr(intPtrSize), hDeviceMemA);
+            error = Cl.SetKernelArg(kernel, 1, new IntPtr(intPtrSize), hDeviceMemB);
+            error = Cl.SetKernelArg(kernel, 2, new IntPtr(intPtrSize), hDeviceMemC);
+
+
+            // write data from host to device
+            Event clevent;
+            error = Cl.EnqueueWriteBuffer(cmdQueue, hDeviceMemA, Bool.True, IntPtr.Zero,
+                new IntPtr(1000 * sizeof(float)),
+                A, 0, null, out clevent);
+            error = Cl.EnqueueWriteBuffer(cmdQueue, hDeviceMemB, Bool.True, IntPtr.Zero,
+                new IntPtr(1000 * sizeof(float)),
+                B, 0, null, out clevent);
+
+            // execute kernel
+            error = Cl.EnqueueNDRangeKernel(cmdQueue, kernel, 1, null, new IntPtr[] { new IntPtr(1000) }, null, 0, null, out clevent);
+            Console.WriteLine($"Run result: {error}");
+            
+            
+
+            error = Cl.EnqueueReadBuffer(cmdQueue, hDeviceMemC, Bool.True, 0, C.Length, C, 0, null, out clevent);
+
+            for (var i=0; i<1000; i++) {
+                Console.WriteLine($"[{i}]: {C[i]}");
+            }
+            
             program.Dispose();
         }
     }
